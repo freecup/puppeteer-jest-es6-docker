ed24f77b913b148352b23f9eca51fc62d60ea09e
Anatoliy <B!x12345>
2020-03-23 14:26:26 +0400

    First Task

---
+++ tests/FirstTask/spec.ts
@@ -0,0 +1,22 @@
+import { singlePack, test } from '@actions'
+import po from '@pages'
+import { browser } from '@config/jest.settings'
+import { CURRENT_DATE } from '@const/global/constants'
+
+singlePack('vyrskiy', () => {
+  test('first task', async () => {
+    const LetuPage = po?.rest
+
+    const path = 'https://www.letu.ru'
+    await LetuPage.open(path, true, undefined)
+
+    await LetuPage.type('#search-form .search-form__input', 'вешалка')
+
+    const searchStatusExpectedText = 'Товары не найдены'
+    const searchStatusLabelText = await LetuPage.getText('.ui-menu-item .search-form-preloader')
+
+    expect(searchStatusLabelText).toEqual(searchStatusExpectedText)
+
+    await browser.close()
+  })
+})
